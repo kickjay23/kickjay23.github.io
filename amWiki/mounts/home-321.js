be81c369978aebce057dbfadf78d171e5bd6726b
kickjay <kickjay@163.com>
2021-02-27 22:18:07 +0800

    Signed-off-by: kickjay <kickjay@163.com>

---
+++ amWiki/mounts/home-321.js
@@ -0,0 +1,1 @@
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['home']={"name":"home-首页.md","path":"home-首页.md","content":"# OCC Wiki\n\n<br>\n\n![欢迎使用知识库！](https://raw.githubusercontent.com/kickjay23/kickjay23.github.io/main/assets/Logo.PNG)  \n\n### 欢迎使用 OCC文库!\n—— **简单，快捷的团队知识库文档**  \n","timestamp":1614429263023}
--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1614316191735}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1614429263023}
