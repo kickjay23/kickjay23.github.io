2d74be9561fbcd6e435e71d4a67dea674e48dc96
kickjay <kickjay@163.com>
2021-02-28 14:56:37 +0800

    Signed-off-by: kickjay <kickjay@163.com>

--- amWiki/mounts/icon-170.js
+++ amWiki/mounts/icon-170.js
@@ -1,1 +1,1 @@
-if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1614429263023}
+if(typeof AWPageMounts=='undefined'){AWPageMounts={}};AWPageMounts['icon']={"name":"icons.svg","path":"../amWiki/images/icons.svg","content":"","timestamp":1614495166451}
--- library/home-首页.md
+++ library/home-首页.md
@@ -2,7 +2,7 @@
 
 <br>
 
-![欢迎使用知识库！](https://raw.githubusercontent.com/kickjay23/kickjay23.github.io/main/assets/Logo.PNG)  
+![欢迎使用知识库！](./assets/Logo.PNG)  
 
 ### 欢迎使用 OCC文库!
 —— **简单，快捷的团队知识库文档**  
